b15339b093b9704e41dcfcf149b31475c4941506
kalibri111 <livan.skin@yandex.ru>
2020-07-26 13:47:03 +0300

    hotfix

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from add_handlers import add_confirmation_handler, add_location_handler,\
     add_place_handler, add_start_handler
 from list_handlers import request_list_handler
 from reser_handlers import reset_handler
+from start_handlers import start_handler
 from settings import bot
 
 

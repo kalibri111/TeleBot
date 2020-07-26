669bbc8314fd8860a8aacca254b583e4506de390
kalibri111 <livan.skin@yandex.ru>
2020-07-26 13:50:45 +0300

    hotfix

--- start_handlers.py
+++ start_handlers.py
@@ -3,7 +3,7 @@ from settings import bot
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    help_str = '/add - добавление места, если ввели неправильно, в конце введите \'нет\'' \
-               '/list - вывод последних 10 мест' \
-               '/reset - удалить все ваши места'
+    help_str = '/add - добавление места, если ввели неправильно, в конце введите \'нет\'\n' \
+               '/list - вывод последних 10 мест\n' \
+               '/reset - удалить все ваши места\n'
     bot.send_message(message.chat.id, help_str)
